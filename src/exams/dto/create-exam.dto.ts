a670ae2b183c46ab68ece051aeb5472ae726a357
luantran2k <tranluan2kyl@gmail.com>
2023-06-23 09:57:05 +0700

    update schema to save score and result of user

--- src/exams/dto/create-exam.dto.ts
+++ src/exams/dto/create-exam.dto.ts
@@ -1,1 +1,12 @@
-export class CreateExamDto {}
+import { Exam } from '@prisma/client';
+
+export class CreateExamDto
+  implements
+    Pick<Exam, 'title' | 'description' | 'duration' | 'type' | 'image'>
+{
+  image: string;
+  title: string;
+  description: string;
+  duration: number;
+  type: string;
+}
